4c2d30d96b378c8f6ef89d53d7be7698fa2c4df8
EC2 Default User <ec2-user@ip-172-31-14-111.ap-northeast-1.compute.internal>
2022-10-03 13:51:21 +0000

    [Add]4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     if book.save
      redirect_to books_path(book.id)
     else
-      render :new
+     render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' =>'books#show'
+  get 'books/:id' =>'books#show', as: 'show_books'
   get 'books/:id/edit' => 'books#edit', as: 'edit_books'
   patch 'books/:id' => 'books#update', as: 'update_books'
-  delete 'books/:id' => 'books#delete', as: 'delete_books'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
